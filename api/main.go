4ea352f7f74b294a7f673f2f50b19051ebef7ff1
christian <hernandezchristian94@gmail.com>
2025-02-21 13:37:46 -0800

    fixed scheduled posts logic

--- api/main.go
+++ api/main.go
@@ -2,9 +2,10 @@ package main
 
 import (
 	"net/http"
+	"pixi/api/handler"
 )
 
 func main() {
-	http.HandleFunc("/", Handler)
+	http.HandleFunc("/", handler.Handler)
 	http.ListenAndServe(":8080", nil)
 }
